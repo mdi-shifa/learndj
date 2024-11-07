fd53d176c8df766488dcec99e948e34b8231abbd
mdi-shifa <152677066+mdi-shifa@users.noreply.github.com>
2024-11-07 23:21:15 +0530

    dj

--- djprojects/calc/views.py
+++ djprojects/calc/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     #function to call homepage and return hello world
-    return HttpResponse("hello world")
+    #we can call any name dynamically
+    return render(request,'home.html',{'name':'shifa'})
