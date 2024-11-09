64db39791e526c1587c6914ad1bcfd13c305e38a
mdi-shifa <152677066+mdi-shifa@users.noreply.github.com>
2024-11-09 20:26:27 +0530

    static 2

--- djprojects/djprojects/settings.py
+++ djprojects/djprojects/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'djprojects.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'calc-templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
