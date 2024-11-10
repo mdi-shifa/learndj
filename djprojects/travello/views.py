59c7354b689147f876c7845ba0f17134ca1d23da
mdi-shifa <152677066+mdi-shifa@users.noreply.github.com>
2024-11-10 19:06:08 +0530

    dynamic data

--- djprojects/travello/views.py
+++ djprojects/travello/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
-
+from .models import destination
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    dest1= destination()
+    dest1.name= 'bangalore'
+    dest1.description= 'the IT city'
+    dest1.img= 'destination_1.jpg'
+    dest1.price= 900
+
+    dest2= destination()
+    dest2.name= 'Kerela'
+    dest2.description= 'a city of nature'
+    dest1.img= 'destination_2.jpg'
+    dest1.price= 700
+
+    dest3= destination()
+    dest3.name= 'hyderabad'
+    dest3.description= 'a city of nawabs'
+    dest1.img= 'destination_3.jpg'
+    dest3.price= 800
+
+    dests=[dest1,dest2,dest3]
+    return render(request,"index.html", {'dests': dests} )
+     
+     
+     
