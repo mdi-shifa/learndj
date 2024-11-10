db548d4dd57bb24719a024e9ea05d555aa64b096
mdi-shifa <152677066+mdi-shifa@users.noreply.github.com>
2024-11-11 00:32:22 +0530

    postgres

--- djprojects/travello/models.py
+++ djprojects/travello/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class destination():
-    id:int
-    name: str
-    description: str
-    img : str
-    price: int
-    offer= bool
+class destination(models.Model):
+    name= models.CharField(max_length=100)
+    description= models.TextField()
+    img = models.ImageField(upload_to='pics')
+    price= models.IntegerField
+    offer= models.BooleanField(default=False)
