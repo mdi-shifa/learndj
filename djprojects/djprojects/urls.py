38d5760b7c3a4367b5d3e53d0157f1361b4ffa08
mdi-shifa <152677066+mdi-shifa@users.noreply.github.com>
2024-11-11 23:46:48 +0530

    add and fetch data

--- djprojects/djprojects/urls.py
+++ djprojects/djprojects/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 # urlpatterns = [
 #     #adding calc in main urls file
@@ -30,3 +32,5 @@ urlpatterns = [
     path('',include('travello.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns= urlpatterns+ static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
--- djprojects/travello/views.py
+++ djprojects/travello/views.py
@@ -1,30 +1,30 @@
 from django.shortcuts import render
 from .models import destination
 # Create your views here.
-def index(request):
-    dest1= destination()
-    dest1.name= 'bangalore'
-    dest1.description= 'the IT city'
-    dest1.img= 'destination_1.jpg'
-    dest1.price= 900
-    dest1.offer= True
+# def index(request):
+#     dest1= destination()
+#     dest1.name= 'bangalore'
+#     dest1.description= 'the IT city'
+#     dest1.img= 'destination_1.jpg'
+#     dest1.price= 900
+#     dest1.offer= True
 
-    dest2= destination()
-    dest2.name= 'Kerela'
-    dest2.description= 'a city of nature'
-    dest2.img= 'destination_2.jpg'
-    dest2.price= 1000
-    dest2.offer= False
+#     dest2= destination()
+#     dest2.name= 'Kerela'
+#     dest2.description= 'a city of nature'
+#     dest2.img= 'destination_2.jpg'
+#     dest2.price= 1000
+#     dest2.offer= False
 
-    dest3= destination()
-    dest3.name= 'hyderabad'
-    dest3.description= 'a city of nawabs'
-    dest3.img= 'destination_3.jpg'
-    dest3.price= 800
-    dest3.offer= True
+#     dest3= destination()
+#     dest3.name= 'hyderabad'
+#     dest3.description= 'a city of nawabs'
+#     dest3.img= 'destination_3.jpg'
+#     dest3.price= 800
+#     dest3.offer= True
 
-    dests=[dest1,dest2,dest3]
-    return render(request,"index.html", {'dests': dests} )
+#     dests=[dest1,dest2,dest3]
+#     return render(request,"index.html", {'dests': dests} )
      
      
      
