64a580f884e008a5089cc2a46dfa70aebd0ac6f4
mdi-shifa <152677066+mdi-shifa@users.noreply.github.com>
2024-11-15 19:44:53 +0530

    user reg p1

--- djprojects/accounts/urls.py
+++ djprojects/accounts/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [ 
     #'' is a call for homepage
-    path('register',views.register, name="register"),
+    path('register/',views.register, name='register'),
 ]
--- djprojects/djprojects/urls.py
+++ djprojects/djprojects/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     #request searching for the index page
     path('',include('travello.urls')),
     path('admin/', admin.site.urls),
-    path(' accounts',include('accounts.urls')),
+    path('accounts/',include('accounts.urls')),
 ]
 
 urlpatterns= urlpatterns+ static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
