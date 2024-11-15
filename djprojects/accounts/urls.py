0f49da4114ed2de3f053b80d330c6a60d2471fe8
mdi-shifa <152677066+mdi-shifa@users.noreply.github.com>
2024-11-16 00:52:01 +0530

    login

--- djprojects/accounts/urls.py
+++ djprojects/accounts/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [ 
     #'' is a call for homepage
     path('register/',views.register, name='register'),
+    path('login/',views.login,name='login')
 ]
--- djprojects/accounts/views.py
+++ djprojects/accounts/views.py
@@ -2,6 +2,24 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.models import User,auth
 # Create your views here.
+
+def login(request):
+    if request.method=='POST':
+        username= request.POST['username']
+        password= request.POST['password']
+        user= auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.error(request,'invalid credentials')
+            return redirect('login')
+
+    else:
+        return render(request,'login.html')
+
+
 def register(request):
 
     if request.method =='POST':
@@ -25,6 +43,7 @@ def register(request):
                 user= User.objects.create_user(username=username,password=password1,first_name=first_name,last_name=last_name)
                 user.save();
                 print('user created')
+                return redirect('login')
              
         else:
             messages.info(request,"password not matching")
